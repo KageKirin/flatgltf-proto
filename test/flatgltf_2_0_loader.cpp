c4784c861fff4103bf55540f1fb4070ae717b85b
KageKirin <kagekirin@gmail.com>
2017-07-06 11:07:46 +0900

    added test loader for flatgltf_2_0

---
+++ test/flatgltf_2_0_loader.cpp
@@ -0,0 +1,51 @@
+//! test fot flatgltf / glTF 2.0 loading
+
+#include "flatgltf/2.0/glTF_generated.h"
+
+#include "flatbuffers/flatbuffers.h"
+#include "flatbuffers/idl.h"
+#include "flatbuffers/util.h"
+
+#include "catch.hpp"
+
+#include <iostream>
+#include <string>
+
+extern const std::string flatgltf_2_0_schema;
+
+int main(int argc, char** argv)
+{
+	if (argc != 2)
+	{
+		std::cerr << argv[0] << " <file>.gltf" << std::endl;
+		return -1;
+	}
+
+	std::string gltfFileContents;
+	bool		gotFile = flatbuffers::LoadFile(argv[1], false, &gltfFileContents);
+
+	if (!gotFile)
+	{
+		std::cerr << argv[1] << " does not seem to exist" << std::endl;
+		return -1;
+	}
+
+
+	flatbuffers::Parser parser;
+	bool				schemaOk = parser.Parse(flatgltf_2_0_schema.c_str(), nullptr, "glTF.fbs");
+	if (!schemaOk)
+	{
+		std::cerr << "schema error: " << parser.error_ << std::endl;
+		return 1;
+	}
+
+	bool gltfOk = parser.Parse(gltfFileContents.c_str(), nullptr, argv[1]);
+	if (!gltfOk)
+	{
+		std::cerr << "json error: " << parser.error_ << std::endl;
+		return 2;
+	}
+
+	std::cout << "gltf json file could be loaded" << std::endl;
+	return 0;
+}
